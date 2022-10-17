7f3524eda8836e66e5fb94b2f65843d346457332
wang_jiangyan@163.com <wang_jiangyan@163.com>
2022-10-17 13:41:25 +0100

    week3 2

---
+++ src/test/LeaseTest.java
@@ -0,0 +1,50 @@
+package test;
+
+import main.exercise_2a.Lease;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.FileAlreadyExistsException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeaseTest {
+
+    Lease testLease;
+
+    @BeforeEach
+    public void createObject() {
+        testLease = new Lease();
+    }
+
+    @Test
+    void getName() {
+        testLease.setName("Jack");
+        assertEquals(testLease.getName(), "Jack");
+        testLease.setName("Brett");
+        assertEquals(testLease.getName(), "Brett");
+    }
+
+    @Test
+    void getRent() {
+        testLease.setRent(105.50);
+        assertEquals(testLease.getRent(), 105.50);
+        testLease.setRent(195.50);
+        assertEquals(testLease.getRent(), 195.50);
+    }
+
+    @Test
+    void getTerm() {
+        testLease.setTerm(12);
+        assertEquals(testLease.getTerm(), 12);
+        testLease.setTerm(12);
+        assertEquals(testLease.getTerm(), 12);
+    }
+
+    @Test
+    void addPetFee() {
+        double total = 105.50 + testLease.getFee();
+        assertEquals(total, 115.50);
+    }
+}
